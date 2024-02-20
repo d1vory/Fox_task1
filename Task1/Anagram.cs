c1c96ac59a2e8af398977a4bd4cec18756da47f6
Olexandr Dubchak <a.dubchak@disoft.dev>
2024-02-20 11:53:22 +0200

    created anagram class and  reverse logic

---
+++ Task1/Anagram.cs
@@ -0,0 +1,42 @@
+using System.Text;
+
+namespace Task1;
+
+public class Anagram
+{
+    public static string ReverseWord(string word)
+    {
+        if (word.Length == 0) return word;
+        StringBuilder resultWord = new StringBuilder();
+        Dictionary<int, char> nonLetterIndices = new Dictionary<int, char>();
+        for (int i = 0; i < word.Length; i++)
+        {
+            char ch = word[i];
+            if (Char.IsLetter(ch))
+            {
+                resultWord.Insert(0, ch);
+            }
+            else
+            {
+                nonLetterIndices.Add(i, ch);
+            }
+        }
+        foreach (var item in nonLetterIndices)
+        {
+            resultWord.Insert(item.Key, item.Value);
+        }
+        return resultWord.ToString();
+    }
+
+    public static string Reverse(string input)
+    {
+        StringBuilder result = new StringBuilder(input);
+        string [] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        foreach (string word in words)
+        {
+            string reversedWord = ReverseWord(word);
+            result.Replace(word, reversedWord);
+        }
+        return result.ToString();
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Task1;
+﻿using System.Text;
+
+namespace Task1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Anagram.Reverse("           a1bcd          efg!h"));
+        // Console.WriteLine(ReverseWord("abcdefg"));
+        // Console.WriteLine(ReverseWord("a1bcd"));
+        // Console.WriteLine(ReverseWord("efg!h"));
+        // Console.WriteLine(ReverseWord("efg!h"));
+        // Console.WriteLine(ReverseWord("efg!h"));
     }
 }
