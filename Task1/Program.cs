f9108a5b0b86e6f88554ef8a4c5f06b11b29b961
Olexandr <dubchakob@gmail.com>
2024-02-20 14:58:26 +0200

    cleanup

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -7,10 +7,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine(Anagram.Reverse("           a1bcd          efg!h"));
-        // Console.WriteLine(ReverseWord("abcdefg"));
-        // Console.WriteLine(ReverseWord("a1bcd"));
-        // Console.WriteLine(ReverseWord("efg!h"));
-        // Console.WriteLine(ReverseWord("efg!h"));
-        // Console.WriteLine(ReverseWord("efg!h"));
     }
 }
